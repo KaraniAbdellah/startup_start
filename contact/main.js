ee254d5346c31cf5629478d78fab4a792777bc18
lahcenbont <rachdilahcen2023@gmail.com>
2025-01-16 17:55:28 +0100

    add features

--- contact/main.js
+++ contact/main.js
@@ -1,1 +1,19 @@
-// start main.js
+// Start Home Page
+
+
+$(document).ready(function () {
+    // Custom the Navbar
+    let navbar = $(".my_navbar");
+    let links = $("ul.links");
+    navbar.click(function () {
+        links.toggleClass("show");
+        if (links.hasClass("show")) {
+            navbar.html(`<i class="fa-solid fa-xmark fs-3"></i>`);
+        } else {
+            navbar.html(`<i class="fa-solid fa-bars fs-3"></i>`);
+        }
+    });
+
+    // Custom the Date [year]
+    let today_year = $("span.date").text(new Date().getFullYear());
+});
