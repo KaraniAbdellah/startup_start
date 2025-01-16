65e22cb09b21f82bfec529b03fe845267c2fa9b4
zakariamoumni9@gmail.com <zakariamoumni9@gmail.com>
2025-01-16 17:55:14 +0100

    worked on the animation using jquery for the background

---
+++ experience/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    // Animate the Testimonials Heading
+    $('#testimonials h2').delay(500).fadeTo(1500, 1).css({
+        transform: 'translateY(0)',
+        opacity: 1
+    });
+
+    // Animate the Projects Heading
+    $('#projects h2').delay(800).fadeTo(1500, 1).css({
+        transform: 'translateY(0)',
+        opacity: 1
+    });
+});
+
+
