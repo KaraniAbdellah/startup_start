c3b9be8b38f4a5b0ecd54ce301b4ab1e871f609e
KaraniAbdellah <abdellahkarani@gmail.com>
2025-01-17 11:43:46 +0100

    add gsap method

--- Learn_Gsap/main.js
+++ Learn_Gsap/main.js
@@ -1,14 +1,68 @@
 // Start Learning GSAP
+/*
+    Timeline:A way to organize animations, like putting them in a list.
+        You can control all animations together (play, pause, reverse).
+    Tween: A single animation, like moving, scaling, or fading an object.
+    
+    A timeline can have many tweens inside it.
+
+
+    from: Starts from a state you set and ends at the current state.
+    to: Starts from the current state and moves to a state you set.
+    fromTo: You set both start and end states.
+
+*/
 
 document.addEventListener("DOMContentLoaded", function (event) {
+    //       x: 300,
+    //       duration: 3,
+    //       ease: "back",
+    //       backgroundColor: "red",
+    //   });
 
-    
+
+    // gsap.from(".code-box", {
+    //     duration: 2,
+    //     x: 100,
+    //     opacity: 0.2
+    // });
+
+
+    //   gsap.fromTo(
+    //     ".code-box",
+    //     {
+    //       opacity: 0,
+    //       scale: 0.5,
+    //       y: 100,
+    //     },
+    //     {
+    //       opacity: 1,
+    //       scale: 1,
+    //       y: 0,
+    //       duration: 0.8,
+    //       stagger: 0.2,
+    //     }
+    //   );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 });
-
-
-
-
-
-
-
