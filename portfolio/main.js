b0551035d0c81ed0a2ab76fcc834fd73aa79da31
mouhamedbouzayan <bouzimohammed177@gmail.com>
2025-01-18 11:44:41 +0100

    add more protfolio feature

--- portfolio/main.js
+++ portfolio/main.js
@@ -17,6 +17,28 @@ $(document).ready(function () {
 
     // Custom the Date [year]
     let today_year = $("span.date").text(new Date().getFullYear());
+
+    // Start Aniamtion For Section Content
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.to(".progress-header", {
+        width: "100%",
+        scrollTrigger: {
+            trigger: document.body,
+            start: "top top",
+            end: "bottom bottom",
+            scrub: true,
+        },
+    });
+
+
+    // show portfolio
+    $(".show_portfolio").on("click", function() {
+        console.log($(".hidden-protfolio-box"))
+        $(".hidden-protfolio-box").each(function() {
+            $(this).removeClass("hidden-protfolio-box");
+            $(".show_portfolio").addClass("hidden-protfolio-box");
+        })
+    });
 });
 
 
