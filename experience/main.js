04d93a9b85253d351042dd841fe156fca2f4701a
KaraniAbdellah <abdellahkarani@gmail.com>
2025-01-19 13:19:54 +0100

    add main_btn

--- experience/main.js
+++ experience/main.js
@@ -1,15 +1,6 @@
 $(document).ready(function () {
     
 
-    AOS.init();
-    
-    // Animate the Projects Heading
-    $('#projects h2').delay(800).fadeTo(1500, 1).css({
-        transform: 'translateY(0)',
-        opacity: 1
-    });
-
-
     // Custom the Navbar
     let navbar = $(".my_navbar");
     console.log(navbar);
@@ -21,31 +12,6 @@ $(document).ready(function () {
         } else {
             navbar.html(`<i class="fa-solid fa-bars fs-3"></i>`);
         }
-
-        document.addEventListener("DOMContentLoaded", function () {
-            const waveSection = document.querySelector('.wave-section');
-            const boxes = document.querySelectorAll('.box');
-
-            // Function to check if the wave-section is in the viewport
-            function checkScroll() {
-                const rect = waveSection.getBoundingClientRect();
-                const isInView = rect.top <= window.innerHeight && rect.bottom >= 0;
-
-                if (isInView && !waveSection.classList.contains('animated')) {
-                    // Trigger animation when the section is in the viewport
-                    waveSection.classList.add('animated');
-                } else if (!isInView && waveSection.classList.contains('animated')) {
-                    // Reset animation when the section is out of view
-                    waveSection.classList.remove('animated');
-                }
-            }
-
-            // Call checkScroll when the page is loaded or scrolled
-            window.addEventListener('scroll', checkScroll);
-            checkScroll(); // Initial check in case the section is already in view on load
-
-
-        });
     });
     
     // Start Aniamtion For Section Content
