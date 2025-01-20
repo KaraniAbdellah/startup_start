3c3b83fe3736c741f78d06bf2977840e4417ddf0
KaraniAbdellah <abdellahkarani@gmail.com>
2025-01-20 10:14:17 +0100

    add animations

--- experience/main.js
+++ experience/main.js
@@ -26,6 +26,43 @@ $(document).ready(function () {
         },
     });
 
+    
+    // Animate Section [Left-Right]
+    gsap.utils.toArray(".leftToRight").forEach((element) => {
+        gsap.from(element, {
+            x: "-50vh",
+            opacity: 0.5,
+            duration: 1,
+            ease: "power2.out",
+            scrollTrigger: {
+                trigger: element,
+                start: "top 60%",
+                end: "top 120%",
+                toggleActions: "play reverse play reverse",
+                scrub: 1
+            }
+        });
+    });
+
+
+    // Animate Section [Bottom-Top]
+    gsap.utils.toArray(".bottomToTop").forEach((element) => {
+        gsap.from(element, {
+            y: "30vh",
+            opacity: 0.5,
+            duration: 3,
+            ease: "power2.out",
+            scrollTrigger: {
+                trigger: element,
+                start: "top 120%",
+                end: "top 100%",
+                toggleActions: "play reverse play reverse",
+                scrub: 1
+            }
+        });
+        });
+
+
 });
 
 
